b602484a7dfae3f1c7ebd844b6d3f627836aabdc
hyvip-ai <rm2932002@gmail.com>
2021-11-11 11:00:31 +0530

    Added Console Text

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+console.log('%cIf You are Here Then you are a Developer!!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
 const introtl = new TimelineLite({paused:true})
