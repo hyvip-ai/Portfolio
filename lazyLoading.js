7f9fab97de10228cb1ecb74d82574438c1325164
hyvip-ai <rm2932002@gmail.com>
2021-11-20 01:49:25 +0530

    Added Lazy Loading

---
+++ lazyLoading.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll('[data-src]');
+const imgOptions = {
+};
+const imgObserver = new IntersectionObserver((entries,imgObserver)=>{
+    entries.forEach(entry=>{
+        if(!entry.isIntersecting){
+            return
+        }
+        else{
+            preLoadImage(entry.target);
+            imgObserver.unobserve(entry.target)
+        }
+    })
+},imgOptions)
+
+const preLoadImage = (img)=>{
+    const src = img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src = src;
+}
+
+
+images.forEach(image=>{
+    imgObserver.observe(image)
+})
